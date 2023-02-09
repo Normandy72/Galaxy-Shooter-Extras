ad5902d894cba8f038d640d96a54c615775a3354
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-02-09 12:58:46 +0100

    add enemy fire

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float _speed = 4f;
+    [SerializeField] private GameObject _laserPrefab;
+
     private Player _player;
     private Animator _animator;
     private AudioSource _audioSource;
+    private float _fireRate = 3.0f;
+    private float _canFire = -1;
 
     void Start()
     {
@@ -32,6 +36,24 @@ public class Enemy : MonoBehaviour
     }
 
     void Update()
+    {
+        CalculateMovement();
+
+        if(Time.time > _canFire)
+        {
+            _fireRate = Random.Range(3f, 7f);
+            _canFire = Time.time + _fireRate;
+            GameObject enemyLaser = Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+            Laser[] lasers = enemyLaser.GetComponentsInChildren<Laser>();
+
+            for(int i = 0; i < lasers.Length; i++)
+            {
+                lasers[i].AssingEnemyLasers();
+            }
+        }
+    }
+
+    private void CalculateMovement()
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -3,8 +3,21 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     [SerializeField] private float _speed = 8f;
+    private bool _isEnemyLaser = false;
 
     void Update()
+    {
+        if(_isEnemyLaser == false)
+        {
+            MoveUp();
+        }
+        else
+        {
+            MoveDown();
+        }
+    }
+
+    private void MoveUp()
     {
         transform.Translate(Vector3.up * _speed * Time.deltaTime);
 
@@ -16,6 +29,39 @@ public class Laser : MonoBehaviour
             }
             
             Destroy(this.gameObject);           
+        }
+    }
+
+    private void MoveDown()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if(transform.position.y < -8f)
+        {
+            if(transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            
+            Destroy(this.gameObject);           
+        }
+    }
+
+    public void AssingEnemyLasers()
+    {
+        _isEnemyLaser = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player" && _isEnemyLaser == true)
+        {
+            Player player = other.GetComponent<Player>();
+
+            if(player != null)
+            {
+                player.Damage();
+            }
         }
     }
 }
